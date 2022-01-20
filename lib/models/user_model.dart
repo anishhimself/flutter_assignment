4762304ecbbec66cfa2513b644b7b7dc3f7feef5
anishhimself <57608042+anishhimself@users.noreply.github.com>
2022-01-20 12:23:29 +0545

    added three models

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -3,10 +3,11 @@ class UserData
   final String userName;
   final String imagePath;
   final String emailAddress;
+  final String password;
   final String contactNumber;
   final String address;
   
 
-  UserData({required this.userName,required this.imagePath,required this.emailAddress,required this.contactNumber,required this.address});
+  UserData({required this.userName,required this.imagePath,required this.emailAddress, required this.password, required this.contactNumber,required this.address});
 
 }
