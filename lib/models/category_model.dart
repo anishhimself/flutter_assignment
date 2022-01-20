b19bff3805a2afc501335a92b72a12a26304e11b
anishhimself <57608042+anishhimself@users.noreply.github.com>
2022-01-20 12:21:22 +0545

    added three models

---
+++ lib/models/category_model.dart
@@ -0,0 +1,9 @@
+class CategoryData
+{
+  final String categoryName;
+  final String imagePath;
+  final int itemQuantity;
+
+  CategoryData({required this.categoryName,required this.imagePath,required this.itemQuantity});
+
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,12 @@
+class UserData
+{
+  final String userName;
+  final String imagePath;
+  final String emailAddress;
+  final String contactNumber;
+  final String address;
+  
+
+  UserData({required this.userName,required this.imagePath,required this.emailAddress,required this.contactNumber,required this.address});
+
+}
